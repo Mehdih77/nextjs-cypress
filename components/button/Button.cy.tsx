3bc7e7be301912aa71b13893b78e96993d114ff2
Mehdi <mehdi.havaei77@gmail.com>
2023-10-21 12:50:04 +0330

    change button component test

--- components/button/Button.cy.tsx
+++ components/button/Button.cy.tsx
@@ -2,16 +2,14 @@ import Button from "./Button";
 
 describe("<Button />", () => {
   it("renders button", () => {
-    cy.mount(<Button />);
+    cy.mount(<Button handleClick={cy.stub()} />);
     cy.get("button").should("have.class", "bg-green-600").as("button");
     cy.get("@button")
       .invoke("hover")
       .should("have.class", "hover:bg-green-700");
-    cy.get("@button").click();
-    cy.get("button").should("contain", "Button clicked"); // cy.get("button").should("include.text", "Button clicked");
   });
-  it.only("disabled button", () => {
-    cy.mount(<Button disabled />);
+  it("disabled button", () => {
+    cy.mount(<Button disabled handleClick={cy.spy()} />);
     cy.get("button").should("have.class", "bg-gray-500");
     // cy.get('button').should('have.css', 'pointer-events', "none") // not clickable
     cy.get("button").click({ force: true });
